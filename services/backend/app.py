1244fa6e10c60e26a3b821e242dece7d116e9268
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-03-19 09:16:40 -0400

    Various backend updates

---
+++ services/backend/app.py
@@ -0,0 +1,52 @@
+# app.py
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    # if key doesn't exist, returns a 400, bad request error
+    framework = request.args['framework']
+
+    # if key doesn't exist, returns None
+    website = request.args.get('website')
+
+    return '''
+              <h1>The language value is: {}</h1>
+              <h1>The framework value is: {}</h1>
+              <h1>The website value is: {}'''.format(language, framework, website)
+
+# allow both GET and POST requests
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form.get('framework')
+        return '''
+                  <h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+               <div><label>Language: <input type="text" name="language"></label></div>
+               <div><label>Framework: <input type="text" name="framework"></label></div>
+               <input type="submit" value="Submit">
+           </form>'''
+
+# GET requests will be blocked
+@app.route('/json-example', methods=['GET'])
+def json_example():
+    return jsonify(
+        username='g.user.username',
+        email='g.user.email',
+        id='g.user.id'
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
