d25c088aa45372cb9319bf5f1c17fd5e499df4a0
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-03-22 10:49:16 -0400

    Various backend updates. Simple Route testing

--- services/backend/app.py
+++ services/backend/app.py
@@ -1,7 +1,81 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, request, flash, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="postgresql://postgres:postgres@db:5432/project_tracker"
+app.config["SECRET_KEY"] = "b'}F\x009\x80\xb9\xb5\x8a\x86\xc0\xe4 9$s\x86\x12\xd2&\x9bB\x0b\xdc"
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+    project_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(length=50))
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    task_id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.project_id'))
+    description = db.Column(db.String(length=50))
+
+    project = db.relationship("Project")
+
+@app.get("/")
+def show_projects():
+    return render_template("index.html", projects=Project.query.all())
+
+
+@app.route("/project/<project_id>")
+def show_tasks(project_id):
+    # When SOmeone visits individual project page
+    return render_template("project-tasks.html", 
+        project=Project.query.filter_by(project_id=project_id).first(),
+        tasks=Task.query.filter_by(project_id=project_id).all()
+    )
+
+@app.route("/add/project", methods=['POST'])
+def add_project():
+    if not request.form['project-title']:
+        flash("Enter a title for your new project", "red")
+    else:
+        project = Project(title=request.form['project-title'])
+        db.session.add(project)
+        db.session.commit()
+        flash("Project added successfully", "Green")
+    return redirect(url_for('show_projects'))
+
+@app.route("/add/task/<project_id>", methods=["POST"])
+def add_task(project_id):
+    # TODO: Add Task
+    if not request.form['task-name']:
+        flash("Enter Task Name", "Red")
+    else:
+        # Add to DB
+        task = Task(description=request.form['task-name'], project_id=project_id)
+        db.session.add(task)
+        db.session.commit()
+        flash("Task Added Successfully", "green")
+    return redirect(url_for("show_tasks", project_id=project_id))
+
+@app.route("/delete/task/<task_id>", methods=["POST"])
+def remove_task(task_id):
+    task = Task.query.get(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    flash("Task Removed Successfully", "green")
+    return redirect(url_for('show_tasks', project_id=task.project_id))
+
+
+@app.route("/delete/project/<project_id>", methods=["POST"])
+def remove_project(project_id):
+    tasks=Task.query.filter_by(project_id=project_id).delete()
+    project = Project.query.get(project_id)
+    db.session.delete(project)
+    db.session.commit()
+
+
+    flash("This project and all associated tasks have been deleted", "green")
+    return redirect(url_for('show_projects'))
 
 @app.route('/query-example')
 def query_example():
