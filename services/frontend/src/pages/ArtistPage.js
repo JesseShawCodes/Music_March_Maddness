51137e50c396567d9e2920e5058fab9fd7e3488a
Jesse Shaw <JesseShawCodes@Gmail.com>
2024-04-15 22:10:54 -0400

    A lot of front end updates. Embedded song preview

--- services/frontend/src/pages/ArtistPage.js
+++ services/frontend/src/pages/ArtistPage.js
@@ -46,6 +46,7 @@ const ArtistPage = () => {
                             {musicQuery.tracks.tracks?.map((key, index) => (
                                 <li className="list-group-item" key={index}>
                                     {key.name} - {key.album.name}
+                                    <iframe style={{borderRadius:12}} src={`https://open.spotify.com/embed/track/${key.id}?utm_source=generator&theme=0`}width="100%" height="152"  allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                                 </li>
                             ))}
                             </ol>
