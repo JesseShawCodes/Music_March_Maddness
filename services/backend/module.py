fcb20d4ee22846658891294059c2e7e1d4641af4
Jesse Shaw <JesseShawCodes@gmail.com>
2024-12-11 12:14:07 -0500

    Test module.py file no longer needed

--- services/backend/module.py
+++
@@ -1,8 +0,0 @@
-# module.py
-'''module.py docstring'''
-def greet(name):
-    '''Test Greeting'''
-    print(f"Hello, {name}!")
-
-if __name__ == "__main__":
-    print("This is the module.py file.")
