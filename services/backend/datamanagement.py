50956ec6260926d3eea3a925887579e7bc25ac7c
Jesse Shaw <JesseShawCodes@gmail.com>
2024-12-12 09:30:04 -0500

    Datamanagement linting

--- services/backend/datamanagement.py
+++ services/backend/datamanagement.py
@@ -1,6 +1,4 @@
-import os
-import requests
-
+'''Various Database related methods'''
 from sqlalchemy import select, desc
 from sqlalchemy.orm import Session
 
@@ -8,11 +6,8 @@ from flask import jsonify
 
 from database import SpotifyAuth, engine
 
-# Import date class from datetime module
-from datetime import date
-
 def get_newest_auth():
-
+    '''Returns the newest available auth method.'''
     with Session(engine) as session:
         smt = select(SpotifyAuth).order_by(desc(SpotifyAuth.time_created)).limit(1)
         result = session.execute(smt)
@@ -21,4 +16,5 @@ def get_newest_auth():
     return f"{most_recent_auth.auth}"
 
 def delete_old_records():
+    '''Delete old records in DB'''
     return jsonify({"Success": True})
