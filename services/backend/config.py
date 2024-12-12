49b5ea11c1ef3cfaf8f46022352acf7273040769
Jesse Shaw <JesseShawCodes@gmail.com>
2024-12-11 21:01:16 -0500

    Config.py docstrings

--- services/backend/config.py
+++ services/backend/config.py
@@ -1,28 +1,30 @@
+'''Config file for app backend.'''
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
+    '''Config defaults'''
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
 
 class ProductionConfig(Config):
+    '''Production Config'''
     DEBUG = False
 
 
 class StagingConfig(Config):
+    '''Staging Config'''
     DEVELOPMENT = True
     DEBUG = True
 
 
 class DevelopmentConfig(Config):
+    '''Development Config'''
     DEVELOPMENT = True
     DEBUG = True
 
 
 class TestingConfig(Config):
+    '''Testing Config'''
     TESTING = True
