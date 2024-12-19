d7f4430cd4cae6d0f3aec21b6398506069646122
Jesse Shaw <JesseShawCodes@gmail.com>
2024-12-19 16:47:58 -0500

    Settings file references environment variables for database

--- services/django_backend/maddness_backend/settings.py
+++ services/django_backend/maddness_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'maddness_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['database_name'],
+        'USER': os.environ['database_user'],
+        'PASSWORD': os.environ['database_password'],
+        'HOST': os.environ['database_host'],
+        'PORT': os.environ['database_port'],  # Default is 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
