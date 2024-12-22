95a1e32595117ef9d400b7420b532d6415ab1368
Jesse Shaw <JesseShawCodes@gmail.com>
2024-12-21 20:30:38 -0500

    Initial get_auth_token methods for spotify app

--- services/django_backend/spotify/apps.py
+++ services/django_backend/spotify/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
-
+import os
+import requests
 
 class SpotifyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spotify'
+
+def get_auth_token():
+    '''Get Spotify Auth Token'''
+    d = {
+      'grant_type': 'client_credentials', 
+      'client_id': os.environ["SPOTIFY_CLIENT_ID"], 
+      'client_secret': os.environ["SPOTIFY_CLIENT_SECRET"]
+      }
+
+    result = requests.post(os.environ["SPOTIFY_AUTH_URL"], data=d, timeout=5).json()
+
+    
