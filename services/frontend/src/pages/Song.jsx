aec00ba6ee2bcc481f9431839db300e78978a716
Jesse Shaw <JesseShawCodes@gmail.com>
2025-01-02 21:00:42 -0500

    Song element styling

--- services/frontend/src/pages/Song.jsx
+++ services/frontend/src/pages/Song.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Song({ song }) {
+  const dimensions = {
+    '{w}': 70,
+    '{h}': 70,
+  };
   return (
-    <li style={{ color: `#${song.attributes.artwork.textColor1}`, backgroundColor: `#${song.attributes.artwork.bgColor}` }}>
-      {song.attributes.name }
-      -
-      {song.attributes.albumName}
+    <li className="d-flex justify-content-between song-row" style={{ color: `#${song.attributes.artwork.textColor1}`, backgroundColor: `#${song.attributes.artwork.bgColor}` }}>
+      <img className="album-cover" src={`${song.attributes.artwork.url.replace(/{w}|{h}/g, (match) => dimensions[match])}`} alt={`${song.attributes.albumName} Album Cover`} />
+      <div>
+        {song.attributes.name }
+        -
+        {song.attributes.albumName}
+      </div>
     </li>
   );
 }
@@ -19,6 +26,7 @@ Song.propTypes = {
       artwork: PropTypes.shape({
         textColor1: PropTypes.string.isRequired,
         bgColor: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
       }),
     }),
   }).isRequired,
