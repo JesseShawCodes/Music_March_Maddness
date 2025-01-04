8659f4df0837ca9c8ccd8b722ced59b02f3fb61e
Jesse Shaw <JesseShawCodes@gmail.com>
2025-01-03 21:37:11 -0500

    Dropped firebase in react code

--- services/frontend/src/index.jsx
+++ services/frontend/src/index.jsx
@@ -3,29 +3,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { initializeApp } from 'firebase/app';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import store from './store';
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
