08a89e565122e7799142330ab4c32be45f8de71f
Jesse Shaw <JesseShawCodes@gmail.com>
2025-01-03 21:33:28 -0500

    Home Page update

--- services/frontend/src/pages/HomePage.jsx
+++ services/frontend/src/pages/HomePage.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function HomePage() {
-  <div className="container app-container">
-    <h1>This is the Home Page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <p>Phasellus at odio a sem fringilla bibendum. Nullam ut imperdiet mi. .</p>
-    <p>In faucibus pretium justo, id commodo velit porttitor nec.</p>
-    <p>Suspendisse in orci vitae lacus cursus rhoncus.</p>
-  </div>;
+function HomePage({ homeHeading = 'This is the Home page', homeContent = 'sfsafs' }) {
+  return (
+    <div className="container app-container">
+      <h1>{homeHeading}</h1>
+      {homeContent}
+    </div>
+  );
 }
 
 export default HomePage;
+
+HomePage.propTypes = {
+  homeHeading: PropTypes.string,
+  homeContent: PropTypes.string,
+};
+
+HomePage.defaultProps = {
+  homeHeading: 'This is the Home page',
+  homeContent: 'fff',
+};
