0264dec2038279a217248c47bbd722c50479b165
Jesse Shaw <JesseShawCodes@gmail.com>
2025-02-22 14:05:28 -0500

    Creeated Matchup and Matchup Song components. Song Matchups are generated after clicking on "Generate Bracket" button

---
+++ services/frontend/src/components/MatchupSong.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function MatchupSong({ song }) {
+  const dimensions = {
+    '{w}': 70,
+    '{h}': 70,
+  };
+
+  return (
+    <div className="w-25" style={{ color: `#${song.attributes.artwork.textColor1}`, backgroundColor: `#${song.attributes.artwork.bgColor}` }}>
+      <img className="album-cover" src={`${song.attributes.artwork.url.replace(/{w}|{h}/g, (match) => dimensions[match])}`} alt={`${song.attributes.albumName} Album Cover`} />
+      {song.attributes.name}
+    </div>
+  );
+}
+
+MatchupSong.propTypes = {
+  song: PropTypes.shape({
+    attributes: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      albumName: PropTypes.string.isRequired,
+      artwork: PropTypes.shape({
+        textColor1: PropTypes.string.isRequired,
+        bgColor: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+      }),
+    }),
+  }).isRequired,
+};
