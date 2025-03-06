261b522c91c321f2363652d4b55993731c83ab1c
Jesse Shaw <JesseShawCodes@gmail.com>
2025-03-06 15:38:30 -0500

    Brackettable refactoring

--- services/frontend/src/components/BracketTable.jsx
+++ services/frontend/src/components/BracketTable.jsx
@@ -1,28 +1,26 @@
-/*eslint-disable*/
+/* eslint-disable */
 import React from 'react';
 
 import Matchup from './Matchup';
 
-import data from './test.json';
-
-function BracketTable({bracket, selectSong}) {
+function BracketTable({ bracket, selectSong, round }) {
   return (
     <>
       {
-        Object.entries(bracket).map(([groupName, matchups]) => (
+        Object.entries(bracket[round]).map(([groupName, matchups]) => (
           <div className={groupName} key={groupName}>
-            <h2 className='mt-1'>{groupName}</h2>
-            <ul className='list-group'>
+            <h2 className="mt-1">{groupName}</h2>
+            <ul className="list-group">
               {matchups.map((matchup) => (
-                  <li className="list-group-item d-flex justify-content-between mb-2 border-bottom" key={matchup[0].id + '_' + matchup[1].id}>
-                    <Matchup
-                      song1={matchup[0]}
-                      song2={matchup[1]}
-                      key={matchup[0].id + '_' + matchup[1].id}
-                      selectSong={selectSong}
-                    />
-                  </li>
-                ))} 
+                <li className="list-group-item d-flex justify-content-between mb-2 border-bottom" key={matchup.song1.groupRank + '_' + matchup.song2.groupRank}>
+                  <Matchup
+                    song1={matchup.song1}
+                    song2={matchup.song2}
+                    key={matchup.song1.song.id + '-' + matchup.song2.song.id}
+                    selectSong={selectSong}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
         ))
@@ -30,7 +28,5 @@ function BracketTable({bracket, selectSong}) {
     </>
   );
 }
-
-// BracketTable.defaultProps = data;
 
 export default BracketTable;
