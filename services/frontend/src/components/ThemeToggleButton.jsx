d4feb99f148afa4d4d200cdfc25aa1aebdc47a93
Jesse Shaw <JesseShawCodes@gmail.com>
2025-03-11 22:29:45 -0400

    Theme styling

--- services/frontend/src/components/ThemeToggleButton.jsx
+++ services/frontend/src/components/ThemeToggleButton.jsx
@@ -1,20 +1,19 @@
-/*eslint-disable*/
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { useTheme } from '../ThemeContext';
 
-const ThemeToggleButton = () => {
+function ThemeToggleButton() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className="form-check form-switch mt-2">
+    <div className="form-check form-switch mt-2 d-flex justify-content-center">
       <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={toggleTheme} />
-      <label className='nav-item' htmlFor="flexSwitchCheckDefault">
-        {theme  === "light" ?  <FontAwesomeIcon icon={faSun} className='text-warning' /> : <FontAwesomeIcon icon={faMoon} />}
+      <label className="nav-item mx-3" htmlFor="flexSwitchCheckDefault">
+        {theme === 'light' ? <FontAwesomeIcon icon={faSun} className="text-warning" /> : <FontAwesomeIcon icon={faMoon} />}
       </label>
     </div>
   );
-};
+}
 
 export default ThemeToggleButton;
