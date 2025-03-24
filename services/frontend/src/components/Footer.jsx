aa916d098057e49c04087feca8952e9495975269
Jesse Shaw <JesseShawCodes@gmail.com>
2025-03-24 10:09:51 -0400

    Footer alignment

--- services/frontend/src/components/Footer.jsx
+++ services/frontend/src/components/Footer.jsx
@@ -1,19 +1,21 @@
 /* eslint-disable */
 import { React } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faLinkedin, faTwitter, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import {
+  faInstagram, faLinkedin, faXTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top navbar-dark">
-        <div className="col-md-4 d-flex align-items-center">
-          <span className="mb-3 mb-md-0">© 2025 Music March Madness</span>
-          <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li className="ms-3"><a className="text-body-secondary" href="#" title="Instagram"><FontAwesomeIcon icon={faInstagram} /></a></li>
-      <li className="ms-3"><a className="text-body-secondary" href="#" title="Linked In"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-      <li className="ms-3"><a className="text-body-secondary" href="#" title="Twitter"><FontAwesomeIcon icon={faXTwitter} /></a></li>
-    </ul>
+      <footer className="py-3 my-4 border-top navbar-dark">
+        <div className="d-flex align-items-center justify-content-between w-100">
+          <div className="mb-3 mb-md-0">© 2025 Music March Madness</div>
+          <ul className="nav col-md-4 list-unstyled">
+            <li className="ms-3"><a className="text-body-secondary" href="#" title="Instagram"><FontAwesomeIcon icon={faInstagram} /></a></li>
+            <li className="ms-3"><a className="text-body-secondary" href="#" title="LinkedIn"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+            <li className="ms-3"><a className="text-body-secondary" href="#" title="Twitter"><FontAwesomeIcon icon={faXTwitter} /></a></li>
+          </ul>
         </div>
       </footer>
     </div>
