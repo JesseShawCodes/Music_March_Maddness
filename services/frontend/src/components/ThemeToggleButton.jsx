622a85cf867a2ad84ad61f0f670d0e4c071bd319
Jesse Shaw <JesseShawCodes@gmail.com>
2025-03-29 12:14:37 -0400

    Light mode label is now white instead of yellow

--- services/frontend/src/components/ThemeToggleButton.jsx
+++ services/frontend/src/components/ThemeToggleButton.jsx
@@ -9,7 +9,7 @@ function ThemeToggleButton() {
   return (
     <div className="form-check form-switch mt-2 d-flex justify-content-center">
       <label className="nav-item mx-3" htmlFor="theme-switch">
-        {theme === 'light' ? <FontAwesomeIcon icon={faSun} className="text-warning" /> : <FontAwesomeIcon icon={faMoon} />}
+        {theme === 'light' ? <FontAwesomeIcon icon={faSun} className="text-light" /> : <FontAwesomeIcon icon={faMoon} />}
         <input className="form-check-input" type="checkbox" role="switch" title="Switch theme" id="theme-switch" onChange={toggleTheme} />
       </label>
     </div>
