63ce403cb067ad9295306c24a3bbd5247e99e246
Jesse Shaw <JesseShawCodes@gmail.com>
2025-04-14 13:00:09 -0400

    Polling update, useRef adjustment. Initial linting cleanup

--- services/frontend/src/pages/ArtistSearch.jsx
+++ services/frontend/src/pages/ArtistSearch.jsx
@@ -1,7 +1,5 @@
-/* eslint-disable */
 import React, { useState, useEffect, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useGetArtistsQuery, useStartSearchMutation, useLazyGetTaskStatusQuery } from '../services/jsonServerApi';
+import { useStartSearchMutation, useLazyGetTaskStatusQuery } from '../services/jsonServerApi';
 
 import Loading from '../components/Loading';
 
@@ -9,9 +7,8 @@ export default function ArtistSearch() {
   const intervalRef = useRef(null);
   const [query, setQuery] = useState('');
   const [taskId, setTaskId] = useState(null);
-  const [checkStatus, setCheckStatus] = useState(null)
 
-  const [startSearch, { isLoading: isSubmitting, error: isError }] = useStartSearchMutation();
+  const [startSearch, { isLoading: isSubmitting }] = useStartSearchMutation();
   const [triggerStatus, { data: statusData }] = useLazyGetTaskStatusQuery();
 
   // Submit handler
@@ -27,41 +24,39 @@ export default function ArtistSearch() {
   // Initiate Polling
   useEffect(() => {
     if (!taskId) return;
-    console.log("Start polling task");
-
-    const interval = setInterval(() => {
-      // debugger;
-      console.log("setInterval!");
-
+    intervalRef.current = setInterval(() => {
       triggerStatus(taskId);
-    }, 2000); // poll every 2 seconds
+    }, 2000);
 
-    return () => clearInterval(interval);
+    return () => {
+      // "Clearing polling interval"
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    };
   }, [taskId, triggerStatus]);
 
   // Turn off Polling
   useEffect(() => {
     if (statusData?.status === 'SUCCESS' || statusData?.status === 'FAILURE') {
-      console.log("STATUS 1")
       clearInterval(intervalRef.current);
     }
-  }, [statusData])
+  }, [statusData]);
 
   const artistList = (res) => {
     return res.results.artists.data.map((artistResult) => (
       <div className="mt-4 mx-4 card border-secondary artist-search-card g-col-6 g-col-md-4" key={artistResult.id}>
-          {
-            Object.prototype.hasOwnProperty.call(artistResult.attributes, 'artwork') ? <img src={artistResult.attributes.artwork.url} className="card-img-top" alt={`${artistResult.attributes.name} promo`} /> : <p> No Image Available</p>
-          }
-          <h2>
-            {artistResult.attributes.name}
-          </h2>
-          <a href={`artist/${artistResult.id}`} className="btn btn-primary" id={artistResult.id}>
-            Choose this artist
-          </a>
+        {
+          Object.prototype.hasOwnProperty.call(artistResult.attributes, 'artwork') ? <img src={artistResult.attributes.artwork.url} className="card-img-top" alt={`${artistResult.attributes.name} promo`} /> : <p> No Image Available</p>
+        }
+        <h2>
+          {artistResult.attributes.name}
+        </h2>
+        <a href={`artist/${artistResult.id}`} className="btn btn-primary" id={artistResult.id}>
+          Choose this artist
+        </a>
       </div>
-    ))
-  }
+    ));
+  };
 
   return (
     <div className="my-4 w-90 mx-auto">
@@ -81,9 +76,11 @@ export default function ArtistSearch() {
 
       {statusData && (
         <div>
-          <p>Status: {statusData.status}</p>
-          <div className="grid d-flex flex-wrap justify-content-center">{statusData.status === "SUCCESS" ? artistList(statusData.result) : <Loading />}</div>
-          {statusData.result && <p>Result: {JSON.stringify(statusData.result)}</p>}
+          <p>
+            Status:
+            {statusData.status}
+          </p>
+          <div className="grid d-flex flex-wrap justify-content-center">{statusData.status === 'SUCCESS' ? artistList(statusData.result) : <Loading />}</div>
         </div>
       )}
     </div>
