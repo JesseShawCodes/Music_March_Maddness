4b8a18d55426319478e9160ae6dd0ce35d94f255
Jesse Shaw <JesseShawCodes@gmail.com>
2025-04-19 23:16:40 -0400

    django_celery_results setup. Dropped unused Celery configurations

--- services/django_backend/madness_backend/celery.py
+++ services/django_backend/madness_backend/celery.py
@@ -7,10 +7,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'madness_backend.settings')
 
 app = Celery('madness_backend')
 
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Discover tasks
+# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 # Prevent worker_state_db crash in production
-app.conf.worker_state_db = None  # Or use '/tmp/celery_worker_state.db'
+# app.conf.worker_state_db = None  # Or use '/tmp/celery_worker_state.db'
+
+def debug_task(self):
+    print(f'Request: {self.request!r}')
--- services/django_backend/madness_backend/settings.py
+++ services/django_backend/madness_backend/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
 
+    'django_celery_results',
+
     # Optional -- requires install using `django-allauth[socialaccount]`.
     'allauth.socialaccount',
     'allauth.socialaccount.providers.apple',
@@ -206,23 +208,23 @@ ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
 
 # Celery Settings
 CELERY_BROKER_URL = os.environ.get("UPSTASH_REDIS_URL")
-CELERY_RESULT_BACKEND = os.environ.get("UPSTASH_REDIS_URL")
+CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
-CELERY_WORKER_PREFETCH_MULTIPLIER = 1
+# CELERY_WORKER_PREFETCH_MULTIPLIER = 1
 
-CELERY_BROKER_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
-CELERY_REDIS_BACKEND_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
+# CELERY_BROKER_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
+# CELERY_REDIS_BACKEND_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
 
-CELERY_RESULT_EXPIRES = 3600
+# CELERY_RESULT_EXPIRES = 3600
 
-CELERY_RESULT_BACKEND_TRANSPORT_OPTIONS = {
-  "ssl_cert_reqs": ssl.CERT_NONE
-}
+# CELERY_RESULT_BACKEND_TRANSPORT_OPTIONS = {
+#  "ssl_cert_reqs": ssl.CERT_NONE
+# }
 
-BROKER_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
+# BROKER_USE_SSL = {"ssl_cert_reqs": ssl.CERT_NONE}
 
-CELERY_REDIS_BACKEND_USE_SSL = {
-  "ssl_cert_reqs": ssl.CERT_NONE
-}
+# CELERY_REDIS_BACKEND_USE_SSL = {
+#  "ssl_cert_reqs": ssl.CERT_NONE
+# }
