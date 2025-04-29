efa280671cbac04bbba4ca9f1bcf145cee3c4d2e
Jesse Shaw <JesseShawCodes@gmail.com>
2025-04-28 22:25:02 -0400

    Minor settings update to handle api configuration from django

--- services/django_backend/madness_backend/settings.py
+++ services/django_backend/madness_backend/settings.py
@@ -105,7 +105,7 @@ REST_FRAMEWORK = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -170,7 +170,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_FILES_DIRS = [BASE_DIR / "static" / "build"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
