93a1351462017c21171b393e9bf62268f171cb12
Jesse Shaw <JesseShawCodes@gmail.com>
2025-05-16 15:10:02 -0400

    Pushing the bracket forward. Some round 1 initial drawing

--- services/frontend/src/components/Bracket.jsx
+++ services/frontend/src/components/Bracket.jsx
@@ -1,40 +1,65 @@
 /* eslint-disable */
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, { useRef, useEffect, useCallback, useContext } from 'react';
+import { Context } from '../context/BracketContext';
 import p5 from 'p5';
 
 const DownloadP5ImageHidden = (bracketDetails) => {
     const p5Ref = useRef(null);
     const offscreenCanvasRef = useRef(null);
+    const value = useContext(Context);
+    const [state] = value;
+
+    const width = 3200;
+    const height = 3800;
+
+    const bracketWeight = 5;
+    const bracketColor = "#000"
+    const textWeight = 1;
+    const matchUpSpace = 60;
+    const matchUpHeight = 160;
+
+    const numTeams = 64;
 
     const generateAndDownload = useCallback(() => {
-        function round1(p, width, height) {
+        function bracketContent(offScreenCanvas, yStart, group, iteration, lineStartX, lineEndX, color, position, height) {
+          console.log("bracketContent");
+          offScreenCanvas.stroke(color);
+          offScreenCanvas.strokeWeight(bracketWeight);
+          offScreenCanvas.line(lineStartX, yStart, lineEndX, yStart);
+          offScreenCanvas.line(lineEndX, yStart, lineEndX, yStart + height);
+          offScreenCanvas.line(lineEndX, yStart + height, lineStartX, yStart + height);
+          offScreenCanvas.text("HELLO", 300, 300);
+        }
+
+        function round1(offScreenCanvas, yStart, color, lineStartX, lineEndX, groupA, groupB, position, height) {
           console.log("ROUND 1");
-          p.fill(0, 0, 100);
-          p.rect(30, 30, 300, 200);
-          p.fill(255);
-          p.text("HEllo", width/3, height/3);
+            let groups = [groupA, groupB]
+            for (let i = 0; i < groups.length; i++) {
+              for (let r = 0; r < groups[i].length; r++) {
+                console.log("HEllo...")
+                bracketContent(offScreenCanvas, yStart, groups[i], r, lineStartX, lineEndX, color, position, height);
+                yStart += matchUpHeight + matchUpSpace;
+              }
+            }
         }
         if (p5Ref.current) {
             const p = p5Ref.current;
-            const width = 3200;
-            const height = 3800;
+
+            const numRounds = Math.log2(numTeams);
+            let rounds = [];
+            let teams = [];
+
             const offscreenCanvas = p.createGraphics(width, height);
             offscreenCanvasRef.current = offscreenCanvas.canvas; // Store the canvas element for potential later use
 
-            // Draw onto the off-screen canvas
-            offscreenCanvas.textSize(36);
-            offscreenCanvas.background(255);
-            offscreenCanvas.fill(0, 0, 1);
-            // offscreenCanvas.rect(50, 50, 300, 200);
-            offscreenCanvas.fill(0);
-            offscreenCanvas.textSize(74);
-            offscreenCanvas.textAlign(p.CENTER, p.CENTER);
-            round1(offscreenCanvas, width, height)
-            offscreenCanvas.fill(0);
-            offscreenCanvas.textStyle(p.BOLD);
-            offscreenCanvas.textSize(24);
-            offscreenCanvas.text(bracketDetails.song, width / 2, height / 2);
+            offscreenCanvas.background(255, 255, 255);
+            
+            offscreenCanvas.strokeWeight(bracketWeight);
+            // round1(offscreenCanvas)
+            // console.log(state.bracket.group1.round1.roundMatchups);
+            round1(offscreenCanvas, 55, bracketColor, 10, 200, state.bracket.group1.round1.roundMatchups, state.bracket.group2.round1.roundMatchups, "left", matchUpHeight)
 
+            // round1(offscreenCanvas, width, height)
             // Trigger the download
             offscreenCanvas.save(`dadgad_${bracketDetails.artistName}_bracket.png`);
         }
@@ -57,7 +82,7 @@ const DownloadP5ImageHidden = (bracketDetails) => {
 
     return (
         <div>
-            <button onClick={handleDownloadClick} class="btn btn-primary">Download your Bracket</button>
+            <button onClick={handleDownloadClick} className="btn btn-primary">Download your Bracket</button>
             {/* You can optionally add a ref to a div if you want p5 to manage its lifecycle there, */}
             {/* but for a hidden download, it's not strictly necessary for the visual rendering. */}
             {/* <div ref={canvasRef} style={{ display: 'none' }} /> */}
