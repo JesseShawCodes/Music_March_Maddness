c337b0c6a06e5071772a8ffb47eefeec5823037b
Jesse Shaw <JesseShawCodes@gmail.com>
2025-06-26 22:26:28 -0400

    Simple Test Setup. Cleaned up HomePage Component

---
+++ services/dadgad/__tests__/index.test.jsx
@@ -0,0 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+// import { render, screen } from "@testing-library/react";
+// import NavBar from "@/app/NavBar";
+
+test('jest is working', () => {
+  expect(true).toBe(true);
+});
