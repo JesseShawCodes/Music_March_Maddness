125e42a1cc41dcb76fb19f3431bb15066c6664a2
Jesse Shaw <JesseShawCodes@gmail.com>
2025-07-21 22:36:25 -0400

    Implement polling functionality in SearchPage using usePollingTask hook

---
+++ services/dadgad/src/app/hooks/usePollingTask.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from "react";
+
+export function usePollingTask(taskId, triggerStatus, data, interval = 2000) {
+  const intervalRef = useRef(null);
+
+  // Initiate Polling
+  useEffect(() => {
+    console.log("Effect 1")
+    if (!taskId) return undefined;
+    intervalRef.current = setInterval(() => {
+      triggerStatus(taskId);
+    }, interval);
+
+    return () => {
+      // "Clearing polling interval"
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    };
+  }, [taskId, triggerStatus, interval]);
+
+  // Clear polling on unmount or when taskId changes
+  useEffect(() => {
+    console.log("EFFECT 2");
+    if (data?.status === 'SUCCESS') {
+      setResults(data.result);
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    } else if (data?.status === 'FAILURE') {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    }
+  }, [data])
+
+  return intervalRef;
+}
