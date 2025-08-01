8d037664fd9eb83b8f7335d84e6cca58747cec43
Jesse Shaw <JesseShawCodes@gmail.com>
2025-08-01 13:50:02 -0400

    feat: Update P5Sketch component dimensions and implement image download functionality for iOS

--- services/dadgad/src/app/components/p5Sketch.jsx
+++ services/dadgad/src/app/components/p5Sketch.jsx
@@ -9,8 +9,7 @@ const Sketch = dynamic(() => import('react-p5'), {
   ssr: false, // This is crucial for p5.js as it needs the browser environment
 });
 
-const P5Sketch = ({ onSketchReady, width = 3200, height = 3000 }) => {
-  // debugger;
+const P5Sketch = ({ onSketchReady, width = 4200, height = 4000 }) => {
   const value = useContext(Context);
   const [state] = value;
   const p5InstanceRef = useRef(null); // Ref to store the p5 instance
@@ -18,7 +17,7 @@ const P5Sketch = ({ onSketchReady, width = 3200, height = 3000 }) => {
   const setup = (p5, canvasParentRef) => {
     p5InstanceRef.current = p5; // Store the p5 instance
     p5.createCanvas(width, height).parent(canvasParentRef);
-    p5.background(0, 120, 0);
+    p5.background(220);
     p5.noLoop();
     
     // Call the callback to pass the p5 instance to the parent component
@@ -28,8 +27,8 @@ const P5Sketch = ({ onSketchReady, width = 3200, height = 3000 }) => {
   };
 
   const draw = (p5) => {
-    // bracket(state, p5, 4000, state.values.artist_image);
-    p5.fill(255); 
+    bracket(state, p5, 4000, state.values.artist_image);
+    // p5.fill(255); 
   };
 
   return (
