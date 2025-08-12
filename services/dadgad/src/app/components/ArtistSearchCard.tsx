69c756b5bb49d3c06b8b8ecf3849bc4cc97bcc28
Jesse Shaw <JesseShawCodes@gmail.com>
2025-08-12 12:12:56 -0400

    Minor changes to ArtistSearchCard component

--- services/dadgad/src/app/components/ArtistSearchCard.tsx
+++ services/dadgad/src/app/components/ArtistSearchCard.tsx
@@ -1,14 +1,26 @@
+import React from "react";
+import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import myImage from '../logo.png';
+import { faMusic } from '@fortawesome/free-solid-svg-icons';
+
 export default function ArtistSearchCard({artistResult}) {
   return (
     <div className="mt-4 mx-4 card artist-search-card g-col-6 g-col-md-4" key={artistResult.id}>
       {
-        Object.prototype.hasOwnProperty.call(artistResult.attributes, 'artwork') ? <img src={artistResult.attributes.artwork.url} className="card-img-top" alt={`${artistResult.attributes.name} promo`} /> : <p> No Image Available</p>
+        Object.prototype.hasOwnProperty.call(artistResult.attributes, 'artwork') ? <img src={artistResult.attributes.artwork.url} className="card-img-top" alt={`${artistResult.attributes.name} promo`} /> : 
+        <div className="h-100 placeholder-image-container d-flex justify-content-center align-items-center">
+          <div className="d-flex flex-column">
+            <Image src={myImage} alt="Dadgad logo" role="presentation" className="me-2" style={{width: '50px', height: 'auto'}}/>
+            <FontAwesomeIcon icon={faMusic} />
+          </div>
+        </div>
       }
       <h2 className='text-center'>
         {artistResult.attributes.name}
       </h2>
       <a href={`artist/${artistResult.id}`} className="btn btn-primary" id={artistResult.id}>
-        Choose this artist
+        Start Bracket
       </a>
     </div>
   )
