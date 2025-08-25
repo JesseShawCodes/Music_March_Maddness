048becb86d904a49d592947fc6d81bdced0085e8
Jesse Shaw <JesseShawCodes@gmail.com>
2025-08-25 16:37:29 -0400

    Changed BracketContext back to jsx

---
+++ services/dadgad/src/app/context/BracketContext.jsx
@@ -0,0 +1,99 @@
+"use client";
+import { createContext, useReducer } from 'react';
+import { uniqueByKey } from '../services/dataService';
+
+export const Context = createContext();
+
+const initialState = {
+  values: [],
+  bracket: {},
+  userBracket: [],
+  championshipBracket: {},
+  round: 1,
+  currentRoundProgres: 0,
+  selectedGroup: 'all',
+  nonGroupPlay: false,
+  finalFour: false,
+  finalTwo: false,
+  champion: undefined,
+  groups: [
+    { id: 1, name: 'group1', progress: null },
+    { id: 2, name: 'group2', progress: null },
+    { id: 3, name: 'group3', progress: null },
+    { id: 4, name: 'group4', progress: null },
+  ],
+};
+
+function bracketReducer(state, action) {
+  switch (action.type) {
+    case 'setValues': {
+      return {
+        ...state,
+        values: action.payload.values,
+      };
+    };
+    case 'setUserBracket': {
+      debugger;
+      let array1 = [...state.userBracket, action.payload.userBracket];
+      let finalArray = uniqueByKey(array1, 'artist')
+      return {
+        ...state,
+        userBracket: [...state.userBracket, action.payload.userBracket],
+      }
+    };
+    case 'setBracket':
+      return {
+        ...state,
+        bracket: action.payload.bracket,
+      };
+    case 'setRound':
+      return {
+        ...state,
+        round: action.payload.round,
+      };
+    case 'setCurrentRoundProgres':
+      return {
+        ...state,
+        currentRoundProgres: action.payload.currentRoundProgres === 1 ? '0' : action.payload.currentRoundProgres,
+      };
+    case 'setSelectedGroup':
+      return {
+        ...state,
+        selectedGroup: action.payload.selectedGroup,
+      };
+    case 'setNonGroupPlay':
+      return {
+        ...state,
+        nonGroupPlay: action.payload.nonGroupPlay,
+      };
+    case 'setFinalFour':
+      return {
+        ...state,
+        finalFour: action.payload.finalFour,
+      };
+    case 'setFinalTwo':
+      return {
+        ...state,
+        finalTwo: action.payload.finalTwo,
+      };
+    case 'setChampionshipBracket':
+      return {
+        ...state,
+        championshipBracket: action.payload.championshipBracket,
+      };
+    case 'setChampion':
+      return {
+        ...state,
+        champion: action.payload.champion,
+      }
+    default:
+      return state;
+  }
+}
+
+export const BracketContext = ({ children }) => {
+  const [state, dispatch] = useReducer(bracketReducer, initialState);
+  return (
+    <Context.Provider value={[state, dispatch]}>{children}</Context.Provider>
+  );
+}
