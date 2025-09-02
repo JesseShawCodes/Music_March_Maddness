6cedf14297f1cea3221cc7e534a4d098f2b65e17
Jesse Shaw <JesseShawCodes@gmail.com>
2025-09-02 15:13:47 -0400

    dropped whatwg-fetch. tsconfig cleanup

--- services/dadgad/jest.polyfill.js
+++ services/dadgad/jest.polyfill.js
@@ -1,5 +1,4 @@
 import { TextEncoder, TextDecoder } from 'util';
-import 'whatwg-fetch'; // adds fetch, Request, Response, Headers
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
